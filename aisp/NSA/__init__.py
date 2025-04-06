1a016734bd937d8a973f50c7ffc664e7bd6a9948
João Paulo <88985821+Joao-Paulo-Silva@users.noreply.github.com>
2025-04-06 14:32:59 -0300

    refactor: Remove score method redundancy from base and derived classes

--- aisp/NSA/__init__.py
+++ aisp/NSA/__init__.py
@@ -1,5 +1,19 @@
+"""Module (NSA) Negative Selection Algorithm
+
+NSAs simulate the maturation process of T-cells in the immune system, where these \
+cells learn to distinguish between self and non-self. Only T lymphocytes capable \
+of recognizing non-self elements are preserved, helping the system detect invaders.
+
+----
+
+Os NSAs simulam o processo de maturação das células-T no sistema imunológico, onde \
+essas células aprendem a distinguir entre o próprio e não-próprio.
+Apenas as linfócitos-T capazes de reconhecer elementos não-próprios são preservadas,\
+ajudando o sistema a detectar invasores.
+
+"""
 from ._negative_selection import BNSA, RNSA
 
 __author__ = "João Paulo da Silva Barros"
 __all__ = ["RNSA", "BNSA"]
-__version__ = "0.1.33"
+__version__ = "0.1.34"
