82bd700d2403ab172d87391e7c5037839677bf33
João Paulo <88985821+Joao-Paulo-Silva@users.noreply.github.com>
2025-04-26 13:01:39 -0300

    feat: translate tests to English and update the version of modified files

--- aisp/utils/tests/test_distance.py
+++ aisp/utils/tests/test_distance.py
@@ -12,12 +12,12 @@ from aisp.utils.distance import euclidean, hamming, cityblock, minkowski
         (np.array([1.0, 2.0, 3.0]), np.array([4.0, 5.0, 6.0]), 5.196),
     ],
     ids=[
-        "Distância Euclidiana - 2 dimensões",
-        "Distância Euclidiana - 3 dimensões",
+        "Euclidean Distance - 2 dimensions",
+        "Euclidean Distance - 3 dimensions",
     ]
 )
 def test_euclidean(u, v, expected_output):
-    """Teste para distância Euclidiana"""
+    """Test for Euclidean distance"""
     result = round(euclidean(u, v), 3)
     assert result == expected_output
 
@@ -29,12 +29,12 @@ def test_euclidean(u, v, expected_output):
         (np.array([True, False, True, False]), np.array([True, True, True, False]), 0.25),
     ],
     ids=[
-        "Distância de Hamming entre vetores binários diferentes Inteiros",
-        "Distância de Hamming entre vetores binários diferentes Boleanos",
+        "Hamming distance between different binary integer vectors",
+        "Hamming distance between different binary Boolean vectors",
     ]
 )
 def test_hamming(u, v, expected_output):
-    """Teste para distância de Hamming"""
+    """Hamming distance test"""
     result = hamming(u, v)
     assert result == expected_output
 
@@ -45,9 +45,13 @@ def test_hamming(u, v, expected_output):
         (np.array([1, 2, 3]), np.array([4, 5, 6]), 3),
         (np.array([0, 0]), np.array([3, 4]), 3.5),
     ],
+    ids=[
+        "Manhattan Distance - 3 dimensions",
+        "Manhattan Distance - 2 dimensions",
+    ]
 )
 def test_cityblock(u, v, expected_output):
-    """Teste para distância Cityblock (Manhattan)"""
+    """Manhattan (Cityblock) Distance Test"""
     result = cityblock(u, v)
     assert result == expected_output
 
@@ -60,12 +64,12 @@ def test_cityblock(u, v, expected_output):
         (np.array([1, 2, 3]), np.array([4, 5, 6]), 3, 1.442),
     ],
     ids=[
-        "Minkowski p=1 - cityblock",
-        "Minkowski p=2 - euclidean",
+        "Minkowski p=1 - Manhattan",
+        "Minkowski p=2 - Euclidean",
         "Minkowski p=3 - Minkowski",
     ]
 )
 def test_minkowski(u, v, p, expected_output):
-    """Teste para distância de Minkowski"""
+    """Minkowski distance test"""
     result = round(minkowski(u, v, p), 3)
     assert result == expected_output
