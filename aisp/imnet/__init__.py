413e394c54f4d39e774a05163f4ecc60f7fc55fc
João Paulo <88985821+Joao-Paulo-Silva@users.noreply.github.com>
2025-06-16 22:35:01 -0300

    feat: launches aiNet training method for development testing

---
+++ aisp/imnet/__init__.py
@@ -0,0 +1,14 @@
+"""Module (IMNET) Immune Network Theory.
+
+This module implements algorithms based on Immune Network Theory proposed by Jerne.
+
+Classes
+-------
+AiNet
+    Artificial Immune Network implementation for clustering.
+"""
+
+from ._artificial_immune_network import AiNet
+
+__author__ = 'João Paulo da Silva Barros'
+__all__ = ['AiNet']
