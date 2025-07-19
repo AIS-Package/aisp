3ed00882afc67961c5a6970ab9d9ca5aaa3eb23b
João Paulo <88985821+Joao-Paulo-Silva@users.noreply.github.com>
2025-07-19 01:40:53 -0300

    feat: add setParams function and unit tests

--- aisp/base/_base.py
+++ aisp/base/_base.py
@@ -1,7 +1,34 @@
-"""Base class for objects with parameter extraction support."""
+"""Base class for parameter introspection compatible with the scikit-learn API."""
 
 
 class Base:
+    """
+    Generic base class for models with a common interface.
+
+    Provides the ``get_params`` and ``set_params`` method for compatibility with
+    the scikit-learn API, allowing access to the model's public parameters.
+    """
+
+    def set_params(self, **params):
+        """
+        Set the parameters of the instance.
+
+        This method is required to ensure compatibility with scikit-learn functions
+
+        Parameters
+        ----------
+        **params
+            set as attributes on the instance.
+
+        Returns
+        -------
+        self
+        """
+        for key, value in params.items():
+            if not key.startswith("_") and hasattr(self, key):
+                setattr(self, key, value)
+        return self
+
     def get_params(self, deep: bool = True) -> dict:  # pylint: disable=W0613
         """
         Return a dictionary with the object's main parameters.
--- aisp/base/_classifier.py
+++ aisp/base/_classifier.py
@@ -13,8 +13,7 @@ from ..utils.metrics import accuracy_score
 class BaseClassifier(ABC, Base):
     """Base class for classification algorithms.
 
-    Defines the abstract methods ``fit`` and ``predict``, and implements the ``score``,
-    ``get_params`` method.
+    Defines the abstract methods ``fit`` and ``predict``, and implements the ``score`` method.
     """
 
     classes: Optional[Union[npt.NDArray, list]] = None
