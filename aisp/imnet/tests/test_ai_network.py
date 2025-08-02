0ab5e95e08839984a48d585d14e19b3ba89762bd
João Paulo <88985821+Joao-Paulo-Silva@users.noreply.github.com>
2025-08-02 00:07:45 -0300

    fix: lint

--- aisp/imnet/tests/test_ai_network.py
+++ aisp/imnet/tests/test_ai_network.py
@@ -9,7 +9,7 @@ from aisp.imnet import AiNet
 
 
 @pytest.fixture
-def b_AiNet_data():
+def b_ai_net_data():
     """Fixture that provides boolean data and a fixed seed for AiNet testing."""
     seed = 123
     np.random.seed(seed)
@@ -18,7 +18,7 @@ def b_AiNet_data():
 
 
 @pytest.fixture
-def r_AiNet_data():
+def r_ai_net_data():
     """Fixture that provides real data and a fixed seed for AiNet testing."""
     seed = 123
     np.random.seed(seed)
@@ -29,18 +29,18 @@ def r_AiNet_data():
 class TestAiNetBinary:
     """Test suite for the AiNet class."""
 
-    def test_fit_and_predict(self, b_AiNet_data):
+    def test_fit_and_predict(self, b_ai_net_data):
         """Should the fit and predict methods of the AiNet class."""
-        X, seed = b_AiNet_data
+        X, seed = b_ai_net_data
         model = AiNet(seed=seed)
         predictions = model.fit_predict(X, verbose=False)
         assert predictions is not None
         assert isinstance(predictions, np.ndarray)
         assert len(predictions) == X.shape[0]
 
-    def test_predict_raises_feature_dimension_mismatch(self, b_AiNet_data):
+    def test_predict_raises_feature_dimension_mismatch(self, b_ai_net_data):
         """Should raise FeatureDimensionMismatch when prediction input has wrong dimensions."""
-        X, seed = b_AiNet_data
+        X, seed = b_ai_net_data
         model = AiNet(seed=seed)
         model.fit(X, verbose=False)
         x_invalid = np.random.choice([True, False], size=(5, 5))
@@ -52,18 +52,18 @@ class TestAiNetBinary:
 class TestAiNetRealValue:
     """Test suite for the AiNet class."""
 
-    def test_fit_and_predict(self, r_AiNet_data):
+    def test_fit_and_predict(self, r_ai_net_data):
         """Should the fit and predict methods of the AiNet class."""
-        X, seed = r_AiNet_data
+        X, seed = r_ai_net_data
         model = AiNet(seed=seed)
         predictions = model.fit_predict(X, verbose=False)
         assert predictions is not None
         assert isinstance(predictions, np.ndarray)
         assert len(predictions) == X.shape[0]
 
-    def test_predict_raises_feature_dimension_mismatch(self, r_AiNet_data):
+    def test_predict_raises_feature_dimension_mismatch(self, r_ai_net_data):
         """Should raise FeatureDimensionMismatch when prediction input has wrong dimensions."""
-        X, seed = r_AiNet_data
+        X, seed = r_ai_net_data
         model = AiNet(seed=seed)
         model.fit(X, verbose=False)
         x_invalid = np.random.random((5, 5))
