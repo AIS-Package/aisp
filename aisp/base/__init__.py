70f8d8f13a8b53cd48e9a5c81fd64380a84a1e97
João Paulo <88985821+Joao-Paulo-Silva@users.noreply.github.com>
2025-08-31 23:58:13 -0300

    feat(base): add base class for optimization algorithms

--- aisp/base/__init__.py
+++ aisp/base/__init__.py
@@ -3,5 +3,6 @@
 from ._base import set_seed_numba
 from ._classifier import BaseClassifier
 from ._clusterer import BaseClusterer
+from ._optimizer import BaseOptimizer
 
-__all__ = ['BaseClassifier', 'BaseClusterer', 'set_seed_numba']
+__all__ = ['BaseClassifier', 'BaseClusterer', 'BaseOptimizer', 'set_seed_numba']
