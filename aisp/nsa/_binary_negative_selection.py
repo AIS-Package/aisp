fix: Corrects the calculation of distances in the class selection function when the sample is not suitable for all detectors, returning a list instead of a float.

aisp/nsa/_binary_negative_selection.py
@@ -238,7 +238,7 @@ class BNSA(BaseClassifier):
 
         class_differences: dict = {}
         for _class_ in self.classes:
-            distances = np.sum(line != self._detectors[_class_]) / self.N
+            distances = np.mean(line != self._detectors[_class_], axis=1)
             # Assign the label to the class with the greatest distance from
             # the nearest detector.
             if self.no_label_sample_selection == "nearest_difference":