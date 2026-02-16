fix(distance): remove incorrect distance normalization

aisp/utils/distance.py
@@ -61,7 +61,7 @@ def euclidean(u: npt.NDArray[np.float64], v: npt.NDArray[np.float64]) -> float64
 
 @njit()
 def cityblock(u: npt.NDArray[float64], v: npt.NDArray[float64]) -> float64:
-    r"""Calculate the normalized Manhattan distance between two points.
+    r"""Calculate the Manhattan distance between two points.
 
     .. math::
         \frac{(|X_{1} - Y_{1}| + |X_{2} - Y_{2}| + \cdots + |X_{n} - Y_{n}|)}{n}
@@ -82,7 +82,7 @@ def cityblock(u: npt.NDArray[float64], v: npt.NDArray[float64]) -> float64:
     if n == 0:
         return float64(-1.0)
 
-    return float64(np.sum(np.abs(u - v)) / n)
+    return float64(np.sum(np.abs(u - v)))
 
 
 @njit()
@@ -120,7 +120,7 @@ def minkowski(
     if n == 0:
         return float64(-1.0)
 
-    return float64((np.sum(np.abs(u - v) ** p) ** (1 / p)) / n)
+    return float64(np.sum(np.abs(u - v) ** p) ** (1 / p))
 
 
 @njit([(types.float64[:], types.float64[:], types.int32, types.float64)], cache=True)

aisp/utils/tests/test_distance.py
@@ -51,8 +51,8 @@ def test_hamming(u, v, expected_output):
 @pytest.mark.parametrize(
     "u, v, expected_output",
     [
-        (np.array([1, 2, 3]), np.array([4, 5, 6]), 3),
-        (np.array([0, 0]), np.array([3, 4]), 3.5),
+        (np.array([1, 2, 3]), np.array([4, 5, 6]), 9),
+        (np.array([0, 0]), np.array([3, 4]), 7),
     ],
     ids=[
         "Manhattan Distance - 3 dimensions",
@@ -68,9 +68,9 @@ def test_cityblock(u, v, expected_output):
 @pytest.mark.parametrize(
     "u, v, p, expected_output",
     [
-        (np.array([0, 0]), np.array([3, 4]), 1, 3.5),
-        (np.array([0, 0]), np.array([3, 4]), 2, 2.5),
-        (np.array([1, 2, 3]), np.array([4, 5, 6]), 3, 1.442),
+        (np.array([0, 0]), np.array([3, 4]), 1, 7),
+        (np.array([0.0, 0.0]), np.array([3.0, 4.0]), 2, 5),
+        (np.array([1, 2, 3]), np.array([4, 5, 6]), 3, 4.327),
     ],
     ids=[
         "Minkowski p=1 - Manhattan",